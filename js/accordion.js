78fa62baa0d6e29bed1cd5fb3df22ac81ca80b64
Marg <paerl.mag@gmail.com>
2021-05-18 13:31:35 +0300

    init: promo page for BMW task

---
+++ js/accordion.js
@@ -0,0 +1,21 @@
+const futureLinksElems = document.querySelectorAll('.feature__link');
+const futureSubElems = document.querySelectorAll('.feature-sub');
+console.log(futureSubElems)
+futureLinksElems.forEach((btn, index) => {
+   btn.addEventListener('click', () => {
+      if (btn.classList.contains('feature__link_active')) {
+         futureSubElems[index].classList.add('hidden');
+         btn.classList.remove('feature__link_active');
+      } else {
+         futureSubElems.forEach((futureSubElem) => {
+            futureSubElem.classList.add('hidden');
+         })
+         futureLinksElems.forEach((futureLinksElem) => {
+            futureLinksElem.classList.remove('feature__link_active');
+         })
+         futureSubElems[index].classList.remove('hidden');
+         btn.classList.add('feature__link_active');
+         console.log(1)
+      }
+   })
+});
